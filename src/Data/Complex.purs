703245e921f24db9ce0c0e1355e2fe88f94b8d64
Sebastian Graf <sgraf1337@gmail.com>
2016-12-08 18:04:54 +0100

    initial commit. lacks EP vizualisation

---
+++ src/Data/Complex.purs
@@ -0,0 +1,122 @@
+module Data.Complex
+  ( Complex ()
+  , Cartesian (..)
+  , Polar (..)
+  , inPolar
+  , outPolar
+  , inCartesian
+  , outCartesian
+  , conjugate
+  , magnitude
+  ) where
+
+import Math (abs, atan2, cos, sin, sqrt)
+import Prelude
+import Data.Generic
+
+
+-- | An abstract complex type.
+data Complex = Complex Number Number
+
+-- | A view used to construct/destruct a complex number using its cartesian form,
+-- | defining the complex number as its real and imaginary parts. The arguments
+-- | of the constructor are:
+-- |
+-- | + The real part of the number.
+-- | + The imaginary part of the number.
+data Cartesian = Cartesian Number Number
+
+-- | A view used to construct/destruct a complex number using its polar form,
+-- | defining the complex numbers as the distance of the number to zero on the
+-- | complex plane and the angle between the x-axis and the number. The arguments
+-- | of the constructor are:
+-- |
+-- | + The angle with the x-axis of the complex plane.
+-- | + The distance to zero.
+data Polar = Polar Number Number
+
+-- | Pattern match on a complex number using its polar form. Example:
+-- |
+-- | ```purescript
+-- | case inPolar z of
+-- |     (Polar angle magnitude) -> ...
+-- | ```
+inPolar :: Complex -> Polar
+inPolar z@(Complex r i) = Polar (atan2 i r) (magnitude z)
+
+-- | Construct a complex number from its polar form. Example: Create the number
+-- | `z = i`
+-- |
+-- | ```purescript
+-- | z :: Complex
+-- | z = outPolar (Polar (pi / 2) 1)
+-- | ```
+outPolar :: Polar -> Complex
+outPolar (Polar a m) = Complex (m * (cos a)) (m * (sin a))
+
+-- | Pattern match on a complex number using its cartesian form. Example:
+-- |
+-- | ```purescript
+-- | case inCartesian z of
+-- |     (Cartesian realPart imaginaryPart) -> ...
+-- | ```
+inCartesian :: Complex -> Cartesian
+inCartesian (Complex r i) = Cartesian r i
+
+-- | Construct a complex number from its cartesian form. Example: Create the number
+-- | `z = 5 + 3i`
+-- |
+-- | ```purescript
+-- | z :: Complex
+-- | z = outCartesian (Cartesian 5 3)
+-- | ```
+outCartesian :: Cartesian -> Complex
+outCartesian (Cartesian r i) = Complex r i
+
+-- | Creates the conjugate of a complex number, with equal real and magnitude but
+-- | with an opposite imaginary part. Example:
+-- |
+-- | ```purescript
+-- | z :: Complex
+-- | z = outCartesian (Cartesian 5 3)
+-- |
+-- | z' :: Complex
+-- | z' = conjugate z
+-- |
+-- | z'' :: Complex
+-- | z'' = outCartesian (Cartesian 5 -3)
+-- |
+-- | z' == z''
+-- | ```
+conjugate :: Complex -> Complex
+conjugate (Complex r i) = Complex r (-i)
+
+-- | Calculates the magnitude of a complex number, which is the distance of the
+-- | number on the complex plan to zero.
+magnitude :: Complex -> Number
+magnitude (Complex r i) = sqrt (r*r + i*i)
+
+-- Instances
+
+derive instance genericComplex :: Generic Complex
+
+-- Number related instances
+instance semiringComplex :: Semiring Complex where
+  add (Complex r i) (Complex r' i') = Complex (r + r') (i + i')
+  zero = Complex zero zero
+  mul (Complex r i) (Complex r' i') = Complex (r*r' - i*i') (r*i' + r'*i)
+  one = Complex one zero
+
+instance ringComplex :: Ring Complex where
+  sub (Complex r i) (Complex r' i') = Complex (r - r') (i - i')
+
+instance commutativeRingComplex :: CommutativeRing Complex
+
+instance eqComplex :: Eq Complex where
+  eq (Complex r i) (Complex r' i') = approxEq r r' && approxEq i i'
+    where
+      epsilon      = 0.00000001
+      approxEq a b = abs (a - b) < epsilon
+
+instance showComplex :: Show Complex where
+  show (Complex r i) = show r <> " + " <> show i <> "i"
---
+++ src/Main.purs
@@ -0,0 +1,195 @@
+module Main where
+
+import Prelude
+import Math (round, pi, sqrt)
+import Data.Complex
+import Data.Ord (comparing)
+import Data.Maybe (Maybe(..), fromJust, fromMaybe)
+import Data.Functor (map)
+import Data.Foldable (maximum, maximumBy, sum)
+import Data.Traversable (sequence)
+import Data.List
+import Data.Int (toNumber, fromNumber)
+import Data.Monoid
+import Data.Generic
+import Flare.Drawing
+import Flare
+import Partial.Unsafe (unsafePartial)
+import Test.FlareCheck
+import Color
+import Color.Scheme.MaterialDesign
+import Debug.Trace
+
+newtype CKPItem
+  = CKPItem
+  { weight :: Complex
+  , value :: Number
+  }
+
+derive instance genericCKPItem :: Generic CKPItem
+
+instance showCKPItem :: Show CKPItem where
+  show = gShow
+
+newtype CKPInstance
+  = CKPInstance
+  { items :: List CKPItem
+  , capacity :: Number
+  }
+
+getInstance (CKPInstance i) = i
+
+derive instance genericCKPInstance :: Generic CKPInstance
+
+instance showCKPInstance :: Show CKPInstance where
+  show = gShow
+
+type EPItem = Number
+
+solveCKP :: CKPInstance -> List CKPItem
+solveCKP (CKPInstance ckp) = unsafePartial solution
+  where
+    solution :: Partial => List CKPItem
+    solution = fromJust <<< maximumBy (comparing knapsackValue) <<< filter isFeasible $ knapsacks
+
+    isFeasible :: List CKPItem -> Boolean
+    isFeasible knapsack = magnitude (knapsackWeight knapsack) <= ckp.capacity
+
+    knapsacks :: List (List CKPItem)
+    knapsacks = powerset ckp.items
+
+knapsackWeight :: List CKPItem -> Complex
+knapsackWeight = sum <<< map (\(CKPItem i) -> i.weight)
+
+knapsackValue :: List CKPItem -> Number
+knapsackValue = sum <<< map (\(CKPItem i) -> i.value)
+
+powerset :: forall a. List a -> List (List a)
+powerset Nil = singleton Nil
+powerset (Cons x xs) = rec <> map (\xs -> x : xs) rec
+  where
+    rec = powerset xs
+
+transformProblem :: List EPItem -> CKPInstance
+transformProblem Nil = CKPInstance {items: Nil, capacity: 1.0}
+transformProblem epItems = CKPInstance {items, capacity}
+  where
+    items :: List CKPItem
+    items = map (\i -> CKPItem {weight: outCartesian (Cartesian i (beta' * (maxWeight - i))), value: 1.0}) epItems
+
+    capacity :: Number
+    capacity = magnitude (knapsackWeight items) / 2.0
+
+    maxWeight :: Number
+    maxWeight = fromMaybe 0.0 (maximum epItems)
+
+    beta' :: Number
+    beta' = beta epItems
+
+beta :: List EPItem -> Number
+beta epInstance = if denom <= 0.0 then 0.0 else sqrt (totalWeight / denom)
+  where
+    maxWeight :: Number
+    maxWeight = fromMaybe 0.0 (maximum epInstance)
+
+    totalWeight :: Number
+    totalWeight = sum epInstance
+
+    n :: Number
+    n = toNumber (length epInstance)
+
+    denom :: Number
+    denom = n*maxWeight - totalWeight
+
+retractSolution :: Int -> List CKPItem -> Maybe (List EPItem)
+retractSolution n optimal = solution
+  where
+    solution =
+      if knapsackValue optimal /= toNumber n /2.0
+      then Nothing
+      else Just (map (\(CKPItem i) -> real i.weight) optimal)
+
+real :: Complex -> Number
+real cplx = case inCartesian cplx of Cartesian r _ -> r
+
+imag :: Complex -> Number
+imag cplx = case inCartesian cplx of Cartesian _ i -> i
+
+solveEP :: List EPItem -> Maybe (List EPItem)
+solveEP items = retractSolution (length items) <<< solveCKP <<< transformProblem $ items
+
+fromReal :: Number -> Complex
+fromReal r = outCartesian (Cartesian r 0.0)
+
+traceShowId :: forall a. Show a => a -> a
+traceShowId a = traceShow a (\_ -> a)
+
+render :: List EPItem -> Drawing
+render items = ckpDrawing <> translate 200.0 0.0 epDrawing
+  where
+    ckpDrawing = ckpBackground <> arrows <> tangent
+    ckpInstance = transformProblem items
+    ckpSolution = solveCKP ckpInstance
+    epSolution = retractSolution (length items) ckpSolution
+    scaleFactor = 100.0/(getInstance ckpInstance).capacity
+    scaledCkpSolution = map (\(CKPItem i) -> conjugate (i.weight * fromReal scaleFactor)) ckpSolution
+    origin = {x: 0.0, y: 150.0}
+    ckpPath = origin : map complexToPoint (scanl (+) (pointToComplex origin) scaledCkpSolution)
+    arrows = outlined (lineWidth 3.0 <> outlineColor green) (path ckpPath)
+    beta' = traceShowId $ beta items
+    n = toNumber (length items)
+    totalWeight = sum items
+    maxWeight = fromMaybe 0.0 (maximum items)
+    p = spy {x: scaleFactor*totalWeight/2.0, y: beta'*scaleFactor*(n*maxWeight-totalWeight)/2.0}
+    right = spy {x: p.x + p.y/beta', y: 150.0}
+    left = spy {x: 0.0, y: 150.0 - p.y - p.x*beta'}
+    tangent = outlined (lineWidth 2.0 <> outlineColor red) (path (left : right : Nil))
+
+    epDrawing = mempty
+
+complexToPoint :: Complex -> Point
+complexToPoint c =
+  { x: real c
+  , y: imag c
+  }
+
+pointToComplex :: Point -> Complex
+pointToComplex {x, y} = outCartesian (Cartesian x y)
+
+ckpBackground :: Drawing
+ckpBackground = sector <> axis
+  where
+    sector = border <> clipped rect circ
+    circ = filled (fillColor kitSeablue50) (circle 0.0 150.0 100.0)
+    outline = outlined (lineWidth 2.0 <> outlineColor black)
+    border = mempty
+          <> outline (path [{x: 0.0, y: 50.0}, {x: 0.0, y: 150.0}, {x: 100.0, y: 150.0}])
+          <> outline (arc 0.0 150.0 (-pi/2.0) 0.0 100.0)
+    axis = outline (path [{x: 0.0, y: 0.0}, {x: 0.0, y: 150.0}, {x: 150.0, y: 150.0}])
+    rect = rectangle 0.0 50.0 100.0 100.0
+    kitSeablue50 = rgb 152 167 197
+
+flare :: forall eff. UI eff Drawing
+flare = render <<< map toNumber <$> sequence itemInput
+  where
+    itemInput =
+      ( int "Item 1" 2
+      : int "Item 2" 2
+      : int "Item 3" 3
+      : int "Item 4" 4
+      : int "Item 5" 4
+      : int "Item 6" 5
+      : Nil)
+
+main = runFlareDrawing "controls" "output" flare
+--main = tests
+
+instance ckpInstanceInteractive :: Interactive CKPInstance where
+  interactive = interactiveShow
+
+
+tests = do
+  flareCheck "transformProblem" transformProblem
+  --flareCheck "solveCKP <<< transformProblem" (solveCKP <<< transformProblem)
+  --flareCheck "knapsackValue <<< solveCKP <<< transformProblem" (knapsackValue <<< solveCKP <<< transformProblem)
+  flareCheck "solveEP" solveEP
